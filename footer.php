d46d4163c8687c53a55bf86ed9c7078098a75764
Iqra916 <noureenmobeen@gmail.com>
2025-09-03 03:35:35 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,7 @@
-</div> <!-- End of content -->
-        </div> <!-- End of content-wrapper -->
-    </div> <!-- End of dashboard-container -->
-    
-    <!-- Overlay for mobile sidebar -->
+</div> 
+        </div> 
+    </div> 
     <div class="overlay"></div>
-    
-    <!-- JavaScript -->
     <script src="../js/main.js"></script>
     <script src="../js/dashboard.js"></script>
 </body>
